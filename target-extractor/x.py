35575381bcee3f01997794bf638684b3ffb6fc23
David Flemström <david.flemstrom@gmail.com>
2018-08-29 18:28:08 +0200

    Support "," in target specifications

--- target-extractor/x.py
+++ target-extractor/x.py
@@ -4,5 +4,6 @@ import sys
 if sys.argv[1] == 'dist':
     for i in xrange(2, len(sys.argv) - 1):
         if sys.argv[i] == '--target':
-            for target in sys.argv[i + 1:]:
-                print target,
+            for target_fragment in sys.argv[i + 1:]:
+                for target in target_fragment.split(','):
+                    print target,
